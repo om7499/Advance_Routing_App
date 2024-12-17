28392c3bd48c999e7b0991e1e4c7dfa5f59e34fd
“om7499” <ombehare8@gmail.com>
2024-12-17 18:49:41 +0530

    Add duild file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This makes sure Vite outputs to the 'build' folder
+  },
+  
+base: "/Advance_Routing_Appg/", // Add this line
+
+
 })
